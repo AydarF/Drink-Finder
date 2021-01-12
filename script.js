2754289d919d4c9b26ab493eb7489ed7833d6fe3
AydarF <aydar_fayzullin@yahoo.com>
2021-01-11 23:39:13 -0500

    Show single drink page

--- script.js
+++ script.js
@@ -47,5 +47,63 @@ function searchDrink(e) {
   }
 }
 
+// Fetch drink by ID
+function getDrinkById(drinkID) {
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const drink = data.drinks[0];
+
+      addDrinkToDOM(drink);
+    });
+}
+
+// Add drink to DOM
+function addDrinkToDOM(drink) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (drink[`strIngredient${i}`]) {
+      ingredients.push(
+        `${drink[`strIngredient${i}`]} - ${drink[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  single_drinkEl.innerHTML = `
+  <div class="single-drink">
+    <h1>${drink.strDrink}</h1>
+    <img src="${drink.strDrinkThumb}" alt="${drink.strDrink}"/>
+    <div class="single-drink-info">
+      ${drink.strCategory ? `<p>${drink.strCategory}</p>` : ""}
+      ${drink.strArea ? `<p>${drink.strArea}</p>` : ""}
+    </div>
+    <div class="main">
+      <p>${drink.strInstructions}</p>
+      <h3>Ingredients</h3>
+      <ul>
+        ${ingredients.map((ing) => `<li>${ing}</li>`).join("")}
+      </ul>
+    </div>
+  </div>`;
+}
+
 // Event listeners
 submit.addEventListener("submit", searchDrink);
+
+drinksEl.addEventListener("click", (e) => {
+  const drinkInfo = e.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains("drink-info");
+    } else {
+      return false;
+    }
+  });
+
+  if (drinkInfo) {
+    const drinkID = drinkInfo.getAttribute("data-drinkid");
+    getDrinkById(drinkID);
+  }
+});
